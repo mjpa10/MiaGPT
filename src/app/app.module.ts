ff6090fdd358453f75d82392929ab26a32f46eec
Matheus José Pereira de Andrade <matheus.andrade@portodorecife.pe.gov.br>
2023-11-30 16:36:15 -0300

    :tada: Commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { ServiceComponent } from './components/service/service.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContatosComponent } from './pages/contatos/contatos.component';
+import { ContatoComponent } from './components/contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ServiceComponent,
+    FooterComponent,
+    ContatosComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
